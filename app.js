2f8b7b137b7359399682ed565b1ab33865c46d1e
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-09-26 14:47:37 -0700

    cookie stand 5

--- app.js
+++ app.js
@@ -110,11 +110,41 @@ var capitolHillShop = {
     for(var i = 0; i < cookiesPerHour.length; i++) {
       var liEl = document.createElement('li');
       liEl.textContent = this.times[i] + cookiesPerHour[i];
-      capitalHill.appendChild(liEl);
+      capitolHill.appendChild(liEl);
     }
   }
 };
 
+var alkiShop = {
+  name: 'Alki',
+  minCustomers: 2,
+  maxCustomers: 16,
+  cookiesPerCustomer: 4.6,
+  times: ['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total: '],
+  generateRandom: function() {
+    return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
+  },
+  generateCookiesPerHour: function() {
+    var cookiesPerHour = [];
+    var totalCookies = 0;
+    for(var i = 0; i < 16; i++) {
+      if(i === 15) {
+        cookiesPerHour.push(totalCookies);
+      } else {
+        cookiesPerHour.push(Math.ceil(this.generateRandom() * this.cookiesPerCustomer));
+        totalCookies += cookiesPerHour[i];
+      }
+    }
+    var alki = document.getElementById('alki');
+    for(var i = 0; i < cookiesPerHour.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = this.times[i] + cookiesPerHour[i];
+      alki.appendChild(liEl);
+    }
+  }
+};
+
+alkiShop.generateCookiesPerHour();
 capitolHillShop.generateCookiesPerHour();
 seattleCenterShop.generateCookiesPerHour();
 firstAndPikeShop.generateCookiesPerHour();
